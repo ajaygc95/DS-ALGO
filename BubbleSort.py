a7fa67c6f3a2bb25f15b84a24244439f1a4fde53
Ajay GC <gcajay95@gmail.com>
2021-02-23 00:58:09 -0800

    More DS and ALGO Brush Up

---
+++ BubbleSort.py
@@ -0,0 +1,12 @@
+__author__ = "Ajay GC"
+
+
+items = [54,26,93,17,77,31,44,55,20]
+
+for i in range(len(items)-1, 0,-1):
+    for j in range(i):
+        if items[i] < items[j]:
+            items[j],items[i] = items[i], items[j]
+
+
+print(items)
--- GraphYoutube.py
+++ GraphYoutube.py
@@ -1,12 +1,12 @@
 __author__ = "Ajay GC"
-
+from queue import *
 
 class Vertex:
     def __init__(self, name):
         self.name = name
         self.neighbour = {}
-        self.distance = 9999
         self.visited = False
+        self.distance = 9999
 
     def add_neighbor(self, nbr, weight):
         self.neighbour[nbr] = weight
@@ -21,55 +21,101 @@ class Graph:
         self.vertices[key] = newvertex
 
     def add_edge(self,first,second, weight):
-        if first in self.vertices and second in self.vertices:
-            for key, value in self.vertices.items():
-                if key == first:
-                    value.add_neighbor(second, weight)
-                if key == second:
-                    value.add_neighbor(first,weight)
-            return True
-        else:
-            return False
-
-    def BFS(self,start):
-        letters = []
-        start.distance = 0
-        start.visited = True
-
-        for item in start.neighbour:
-            self.vertices[item].distance = start.distance +1
-            letters.append(item)
-
-        while len(letters)>0:
-            new_item = letters.pop(0)
-            node_new_item = self.vertices[new_item]
-            node_new_item.visited = True
-
-
-            for third_item in node_new_item.neighbour:
-                node_third_item = self.vertices(node_third_item)
-                letters.append(third_item)
-                if not node_third_item:
-                    letters.append(third_item)
-                    if node_third_item > node_new_item +1:
-                        node_third_item = node_new_item +1
-
+        if first not in self.vertices:
+            self.add_vertex(first)
+        elif second not in self.vertices:
+            self.add_vertex(second)
+        for key, value in self.vertices.items():
+            if key == first:
+                value.add_neighbor(second, weight)
+            if key == second:
+                value.add_neighbor(first,weight)
 
     def view(self):
         for key in self.vertices.keys():
-            print(self.vertices[key].neighbour,self.vertices[key].distance)
+            print(self.vertices[key].neighbour)
 
-vertex = Vertex('A')
-a =  Graph()
-for i in range(ord('A'), ord('K')):
-    a.add_vertex(chr(i))
+    def BFS(self,start):
+        nexttovisit = []
+        visited = {}
+        nexttovisit.append(start)
+        self.vertices[start].visited = True
+        while nexttovisit:
+            value = nexttovisit.pop(0)
+            print(value, end='-->')
+            node_value = self.vertices[value]
+            for key, value in node_value.neighbour.items():
+                node_key = self.vertices[key]
+                if node_key.visited == False:
+                    nexttovisit.append(key)
+                    node_key.visited = True
 
-edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
+graphA = Graph()
+graphA.add_vertex('A')
+graphA.add_vertex('B')
+graphA.add_edge('A','B',5)
+graphA.add_edge('A','C',3)
+graphA.add_edge('B','C',6)
+graphA.add_edge('B','D',3)
+graphA.add_edge('C','D',2)
+# graphA.view()
+graphA.BFS('A')
 
-count = 1
-for edge in edges:
-    a.add_edge(edge[:1], edge[1:],count)
-    count += 1
 
-a.BFS(vertex)
-a.view()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def BFS(self,start):
+    #     d
+
+    # def BFS(self,start):
+    #     letters = []
+    #     start.distance = 0
+    #     start.visited = True
+    #
+    #     for item in start.neighbour:
+    #         self.vertices[item].distance = start.distance +1
+    #         letters.append(item)
+    #
+    #     while len(letters)>0:
+    #         new_item = letters.pop(0)
+    #         node_new_item = self.vertices[new_item]
+    #         node_new_item.visited = True
+    #
+    #
+    #         for third_item in node_new_item.neighbour:
+    #             node_third_item = self.vertices(node_third_item)
+    #             letters.append(third_item)
+    #             if not node_third_item:
+    #                 letters.append(third_item)
+    #                 if node_third_item > node_new_item +1:
+    #                     node_third_item = node_new_item +1
+
+
+
+# vertex = Vertex('A')
+# a =  Graph()
+# for i in range(ord('A'), ord('K')):
+#     a.add_vertex(chr(i))
+#
+# edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
+#
+# count = 1
+# for edge in edges:
+#     a.add_edge(edge[:1], edge[1:],count)
+#     count += 1
+#
+# a.BFS(vertex)
+# a.view()
