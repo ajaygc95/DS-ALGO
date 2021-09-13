30dc01b679c787fe2a99c9d3dc363a030de5c056
gcajay95 <gcajay95@gmail.com>
2021-09-13 00:19:07 -0700

    kth largest

--- Algorithms/kth_largest_element.py
+++ Algorithms/kth_largest_element.py
@@ -1,35 +1,54 @@
 
 def swap(i,j , arr):
-    print(f"swapping {i} {j}")
+    print(f" index:{i} {j}  swapping {arr[i]}, {arr[j]}")
     arr[i], arr[j] = arr[j], arr[i]
 
-def partition(start, end, arr):
+
+def partition(start, end, array):
+    pivot_i = start
+    pivot = array[pivot_i]
 
     i = start
     j = end
-
-    pivot_i = start
-    pivot = arr[start]
-
+    
     while i < j:
-        print(arr)
-        while i < len(arr) and arr[i] <= pivot:
+        print(i,j)
+        print(array)
+        while i < len(array) and array[i] <= pivot:
             i += 1
-
-        while j > start and arr[j] > pivot:
-            j =- 1
+            print(f"i: index: {i}, value: {array[i]}")
+        
+        while array[j] >= pivot:
+            j -= 1
+            print(f"j: index: {j}, value: {array[j]}")
         
         if i < j:
-            swap(i,j ,arr)
+            swap(i, j, array)
 
-    swap(start, pivot_i, arr)
+        print(i,j)
 
-    return end
+    print(f"pivot element: {array[i]}")
+    print(f"i: {i} j: {j} pivot_i: {pivot}")
 
-def kth_largest_element(arr, k):
-    pass
+    swap(j, pivot_i,array)
+    print(array)
+    return j
 
 
-input = [5, 1, 10, 3, 2]
-print(partition(0, len(input)-1, input))
-print(input)
+def quick_sort(nums, k):
+
+    pos = partition(0, len(nums)-1, nums)
+    if pos > len(nums) - k:
+        print("==========================",k-(len(nums)-pos)) # 3 - (6-3)
+        return quick_sort(nums[:pos], k-(len(nums)-pos))
+    elif pos < len(nums) - k:
+        return quick_sort(nums[pos+1:], k)
+    else:
+        return nums[pos]
+    
+
+
+input = [5, 1, 7,  10, 3, 2, 11]
+
+fonal = quick_sort(input, 3)
+print("this is final:" ,fonal)
