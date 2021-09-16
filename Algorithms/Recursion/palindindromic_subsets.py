f7dc9dec8e62fdad60e2847188b6832a551492bb
gcajay95 <gcajay95@gmail.com>
2021-09-16 00:37:41 -0700

    palindromic Subsets

--- Algorithms/Recursion/palindindromic_subsets.py
+++ Algorithms/Recursion/palindindromic_subsets.py
@@ -1,29 +1,40 @@
-real_final = []
-
-def palindromic(input,i, subsets):
-
-    if len(input) == i:
-        w = ""
-        for i in subsets:
-            w = i + w
-        print("".join(subsets) ,w)
-
-        if "".join(subsets) == w:
-            print(subsets)
-            final_word = ''.join(subsets)
-        else:
-            final_word = '|'.join(subsets)
-        real_final.append(final_word)
-        return
-    
-    palindromic(input, i+1, subsets)
-    subsets += input[i]
-    palindromic(input, i+1, subsets)
-    subsets.pop()
 
 
-input = "abracadabra"
+def is_pal(word, left, right):
+    print(f"pos: {left} --> '{word[left]}' and i: {right} -->'{word[right]}'")
+    while left < right:
+        if word[left] is not word[right]:
+            return False
+            left += 1
+            right += 1
+        return True
 
-palindromic(input, 0, [])
+real_final   = []
+def palindromic(input, pos, subsets, current_decompostition):
 
-print(real_final)
+    if pos == len(input):
+        print("hello")
+        subsets.append(input[pos])
+        return 
+
+    for i in range(pos,len(input)):
+        print(f"pos: {pos}")
+        
+        #checking if they are palindrome
+
+        if is_pal(input,pos, i):
+            if pos == 0:
+
+                #we are adding input[0:i] so do not put '|' before it. 
+                palindromic(input, pos+1, subsets, input[pos:i-pos+1])
+            
+            else:
+                palindromic(input, pos+1, subsets, input[pos:i-pos+1])
+
+    return subsets
+
+input = "abar"
+
+print(palindromic( input, 0, real_final, ""))
+
+
