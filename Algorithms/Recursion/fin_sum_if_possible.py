a8fe8cecbbd28ed169f7a06b4a70aaa24105b505
gcajay95 <gcajay95@gmail.com>
2021-09-18 20:08:02 -0700

    target sum of subsets

---
+++ Algorithms/Recursion/fin_sum_if_possible.py
@@ -0,0 +1,61 @@
+def subsetsum(S,k):
+    return helper(S,0,k,[],0)
+
+def helper(s, pos, k, slate, sum):
+    if sum > k:
+        return 0
+    if sum == k:
+        return 1
+    if pos == len(s):
+        return 0
+    else:
+        return helper(s, pos+1, k, slate, sum) + helper(s, pos+1, k, slate + [s[pos]],sum+s[pos])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def find_something(array, pos, setsum, k , items=[]):
+    
+#     if pos == len(array):
+#         if sum(setsum) == k:
+#             items += setsum
+#             return True
+#         return 
+#     else:
+#         find_something(array, pos+1, setsum, k) #exclude
+#         setsum += [array[pos]]
+#         find_something(array, pos+1, setsum, k)#include
+#         setsum.pop() 
+
+#     return items
+
+# input = [1,2,3,4,7]
+# k = 11
+# to_print = find_something(input, 0, [], k)
+
+# print(to_print)
+
+
+
