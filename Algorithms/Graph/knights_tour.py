98b0d32ebb867b4815dd5361fa82a4825a7f85d5
gcajay95 <gcajay95@gmail.com>
2021-09-30 19:38:47 -0700

    knights_tour

---
+++ Algorithms/Graph/knights_tour.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+DIRECTIONS = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+def get_nbr(r, c):
+    
+    neighbour = []
+    
+    for dr, dc in DIRECTIONS:
+        new_r, new_c = r+dr, c+dc
+        
+        if new_r <0 or new_r >= 3 
+
+        neighbour.append((new_r,new_c))
+
+    return neighbour
+
+print(get_nbr(0,0))
