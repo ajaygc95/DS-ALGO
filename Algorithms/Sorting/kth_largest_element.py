27e04e3a7f8e79473ee5bb82e87695706345290b
gcajay95 <gcajay95@gmail.com>
2021-10-01 23:41:42 -0700

    Revisign Stuff

--- Algorithms/Sorting/kth_largest_element.py
+++ Algorithms/Sorting/kth_largest_element.py
@@ -32,23 +32,43 @@ def partition(start, end, array):
 
     swap(j, pivot_i,array)
     print(array)
+
     return j
 
 
-def quick_sort(nums, k):
 
-    pos = partition(0, len(nums)-1, nums)
-    if pos > len(nums) - k:
-        print("==========================",k-(len(nums)-pos)) # 3 - (6-3)
-        return quick_sort(nums[:pos], k-(len(nums)-pos))
-    elif pos < len(nums) - k:
-        return quick_sort(nums[pos+1:], k)
-    else:
-        return nums[pos]
+def quick_sort(input,start,end,k):
+
+    if start<end:
+        pivot = partition(start,end,input)
+
+
+        print("this is pivot")
+
+
     
 
 
 input = [5, 1, 7,  10, 3, 2, 11]
 
-fonal = quick_sort(input, 3)
+fonal = quick_sort(input,0,len(input)-1, 3)
 print("this is final:" ,fonal)
+
+
+
+
+
+
+
+
+
+# def quick_sort(nums, k):
+
+#     pos = partition(0, len(nums)-1, nums)
+#     if pos > len(nums) - k:
+#         print("==========================",k-(len(nums)-pos)) # 3 - (6-3)
+#         return quick_sort(nums[:pos], k-(len(nums)-pos))
+#     elif pos < len(nums) - k:
+#         return quick_sort(nums[pos+1:], k)
+#     else:
+#         return nums[pos]
--- Algorithms/Sorting/quick_sort.py
+++ Algorithms/Sorting/quick_sort.py
@@ -1,37 +1,93 @@
 
 
-def swap(a, b, arr):
+def swap(arr, a, b):
     arr[a], arr[b] = arr[b], arr[a]
 
 
-def partition(start, end, arr):
-    
+
+
+input = [ 10, 7, 8, 9, 1, 5 ]
+
+
+def quicksort(input, start, end, k):
+
+    if start < end:
+        pivot = partition(input, start, end)
+
+        if k == pivot:
+            print("k found")
+            return input[pivot]
+
+        if k >pivot:
+                quicksort(input, pivot+1,end, k)
+        elif k <pivot:
+            quicksort(input, start, pivot-1, k)
+            
+    return None
+
+
+def partition(array, start,end):
     pivot_i = start
-    pivot = arr[pivot_i]
+    pivot = array[start]
+
 
     while start < end:
-        while start < len(arr) and arr[start] <= pivot:
+
+
+        while start < len(array) and array[start] <= pivot:
             start += 1
-        while arr[end] > pivot:
-            end -= 1
+        
+        while array[end] > pivot:
+            end -= 1 
 
         if start < end:
-            swap(start, end, arr)
+            swap(array, start, end)
+        
+    swap(array, pivot_i, end)
 
-
-    print("last swap", arr[end], pivot)
-    swap(end, pivot_i, arr)
-    print(f"after partitionaing : {arr}")
     return end
 
-def quicksort(start, end, array):
-    print(array, start, end)
-    if start < end:
-        pivot = partition(start, end, array)
-        quicksort(start, pivot-1, array)
-        quicksort(pivot+1, end, array)
+def overall(input):
+    quicksort(input,0,len(input)-1,3)
 
 
-main_list = [ 10, 7, 8, 9, 1, 5 ]
-quicksort(0, len(main_list)-1, main_list)
-print(f"after quick sorting: {main_list}")
+print(overall(input))
+print(input)
+
+
+
+
+
+
+
+
+# def partition(start, end, arr):
+    
+#     pivot_i = start
+#     pivot = arr[pivot_i]
+
+#     while start < end:
+#         while start < len(arr) and arr[start] <= pivot:
+#             start += 1
+#         while arr[end] > pivot:
+#             end -= 1
+
+#         if start < end:
+#             swap(start, end, arr)
+
+
+#     print("last swap", arr[end], pivot)
+#     swap(end, pivot_i, arr)
+#     print(f"after partitionaing : {arr}")
+#     return end
+
+# def quicksort(start, end, array):
+#     print(array, start, end)
+#     if start < end:
+#         pivot = partition(start, end, array)
+#         quicksort(start, pivot-1, array)
+#         quicksort(pivot+1, end, array)
+
+
+# quicksort(0, len(main_list)-1, main_list)
+# print(f"after quick sorting: {main_list}")
