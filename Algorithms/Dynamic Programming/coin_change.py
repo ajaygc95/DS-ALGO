21823ff152ed6a9cf91bfe531c35ee1a99b8d71d
gcajay95 <gcajay95@gmail.com>
2021-11-07 22:50:55 -0800

    coin change problem

---
+++ Algorithms/Dynamic Programming/coin_change.py
@@ -0,0 +1,20 @@
+a = 63
+coins = [1,5,10,21,25]
+
+dptable = [float('inf')] * (a+1)
+
+dptable[0] = 0
+
+
+for i in range(1 ,a+1):
+
+    for coin in coins:
+        if i-coin >= 0:
+
+            dptable[i] = min(dptable[i], dptable[i-coin])
+    #this one is for adding 1 for itself , ME value. lol
+    
+    dptable[i] += 1
+    
+print(dptable)
+print(dptable[a])
