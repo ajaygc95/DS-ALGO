c1cd4ceb970d19625240f57260bc7156869f4cd5
gcajay95 <gcajay95@gmail.com>
2021-11-14 00:23:20 -0800

    DP Sat

--- Algorithms/Dynamic Programming/number_of_way_to_make_change.py
+++ Algorithms/Dynamic Programming/number_of_way_to_make_change.py
@@ -1,20 +1,15 @@
+
 input = [1,2,3]
 amount  = 3
 
-
 dptable= [0]*(amount+1)
+dptable[0] = 1
 
-count = 0
-
-for i in range(1,len(dptable)):
-
+for i in range(1, len(dptable)):
 
     for coin in input:
-        if i - coin >= 0:
+        if coin >=0:
+            dptable[i] = dptable[i-coin] + dptable[i]
 
-            if i -coin == 0:
-                dptable[i] = dptable[i-1] + 1
+print(dptable[-1])
 
-
-
-print(dptable[i])
