f070281773b2e46c1eb22e06f33a037d56b6887d
gcajay95 <gcajay95@gmail.com>
2021-11-16 19:08:38 -0800

    Recursion Tuesday

--- Algorithms/Searching and Sorting/dutch_flag.py
+++ Algorithms/Searching and Sorting/dutch_flag.py
@@ -1,28 +1,75 @@
-given_liset =  ["B", "R"]
+given_list =  ["B", "R","G", "B",'R',"G"]
 
-def swap(a,b,array):
+def swap(array, a, b):
     array[a], array[b] = array[b], array[a]
 
-def dutch_sort(balls):
 
-    i = 0
-    g = -1
-    r = -1
+def helper(arr):
 
-    while i < len(balls):
-        if balls[i] == "G":
-            g += 1 #0
-            swap(i,g,balls)
-        
-        if balls[i] == "R":
-            g += 1
-            swap(i,g,balls)
+    r = 0
+    g = 0
+    b = -1
 
+    for i in range(len(arr)):
+
+        if arr[i] == "R":
+            swap(arr, i, r)
             r += 1
-            swap(r,g,balls)
-        i += 1
-dutch_sort(given_liset)
-print(given_liset)
+        
+        if arr[i] == "G":
+
+
+helper(given_list)
+
+print(given_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def dutch_sort(balls):
+
+#     i = 0
+#     g = -1
+#     r = -1
+
+#     while i < len(balls):
+#         if balls[i] == "G":
+#             g += 1 #0
+#             swap(i,g,balls)
+        
+#         if balls[i] == "R":
+#             g += 1
+#             swap(i,g,balls)
+
+#             r += 1
+#             swap(r,g,balls)
+#         i += 1
+# dutch_sort(given_liset)
+
             
 
 
--- Algorithms/Searching and Sorting/two_sum.py
+++ Algorithms/Searching and Sorting/two_sum.py
@@ -1,19 +1,67 @@
-given_list =[1, 4, 3, 5, 10]
+given_list =[1, 4, 3, 2, 5, 10]
 target = 7
 
-def pair_sum_sorted_array(numbers, target):
+globalbox= []
+
+def helper(arr, target):
 
     start = 0
-    right =  len(numbers)-1
+    end = len(arr)-1
+    adjlist ={}
 
-    while start <= right:
 
-        if numbers[start] + numbers[right] == target:
-            return [start,right]
+    while start < end:
+        print(adjlist)
+        remaining = target- arr[start]
 
-        if numbers[start] + numbers[right] > target:
-            right -= 1
-        else:
-            start += 1
+        if remaining in adjlist:
+            globalbox.append((adjlist[remaining],start))
+            return
+        adjlist[arr[start]] = start
 
-print(pair_sum_sorted_array(given_list,target))
+        start +=1
+
+
+helper(given_list,target)
+print(globalbox)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def pair_sum_sorted_array(numbers, target):
+
+#     start = 0
+#     right =  len(numbers)-1
+
+#     while start <= right:
+
+#         if numbers[start] + numbers[right] == target:
+#             return [start,right]
+
+#         if numbers[start] + numbers[right] > target:
+#             right -= 1
+#         else:
+#             start += 1
+
+# print(pair_sum_sorted_array(given_list,target))
