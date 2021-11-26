4e3c5cb3681392f81bc99eb54ae0a7283ca357c6
gcajay95 <gcajay95@gmail.com>
2021-11-26 00:01:03 -0800

    thanksgiving Graph

--- Algorithms/Graph/string_tranformation.py
+++ Algorithms/Graph/string_tranformation.py
@@ -1,30 +1,67 @@
 
-
-
-
-
-
-# adj_list = [[0]*len(input[0]) for _ in range(len(input))]
-
-# for i in range(len(input)):
-#     for j in range(len(input[0])):
-#         adj_list[i][j] = input[i][j]
-
-
-#create a set
-#create a queue
-#find length of queue in each level
-
 from collections import deque
 
-input = ["cat", "hat", "bad", "had"]
+words = ["cat", "hat", "bad", "had",]
 start = "bat"
 stop = "had"
 
+aux = {}
+input = words + ["bat"]
+
+print(input)
+for item in input:
+
+    for i in range(len(item)):
+
+        if item.replace(item[i],"*") not in aux:
+            aux[item.replace(item[i],"*")] = [item]
+        aux[item.replace(item[i],"*")].append(item)
 
 
+final = {}
+for item in input:
+    final[item] = set()
+
+for item in input:
+    for i in range(len(item)):
+        for j in aux[item.replace(item[i],"*")]:
+            if j != item:
+                final[item].add(j)
 
 
+for item in final.items():
+    print(item)
+
+print()
+
+visited = {item:False for item in input}
+print(visited)
+globalbox = [0]
+parent = {}
+def helper(start, end):
+
+    if start == end:
+        return start
+
+    visited[start] = True
+
+    q = deque([start])
+    path = []
+    while q:
+
+        for _ in range(len(q)):
+            new_node = q.popleft()
+
+
+            for nbr in final[new_node]:
+                print("hello")
+                if not visited[nbr]:
+                    parent[nbr] = new_node
+                    q.append(nbr)
+                    visited[nbr] = True
+
+
+# print(min(globalbox))
 
 
 
