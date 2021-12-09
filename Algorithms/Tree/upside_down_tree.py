bd91a5e441f6df77406c1dd9280ac1801d309943
gcajay95 <gcajay95@gmail.com>
2021-12-08 18:22:05 -0800

    upside down

--- Algorithms/Tree/upside_down_tree.py
+++ Algorithms/Tree/upside_down_tree.py
@@ -37,19 +37,31 @@ class Node:
 node1 = Node(1)
 node1.left = Node(2)
 node1.right = Node(3)
-# node1.right.right = Node(5)
-# node1.left.left = Node(5)
+node1.left.right = Node(5)
+node1.left.left = Node(4)
 # node1.left.right = Node(5)
 
 def helper(root):
 
-    if not root.left and not root.right:
-        return 
+    if not root.left:
+        return root
     
-    new_root = Node(helper(root.left))
+    new_root = helper(root.left)
+    
+    root.left.left = root.right
+    root.left.right = root
+
+    root.right = None
+    root.left = None
+
+    # new_root.left = root.right
+    # new_root.right = root
+    # root.right = None
+    # root.left = None
 
     return new_root
 
 
-store = helper(node1)
 
+store = helper(node1)
+store.in_order()
