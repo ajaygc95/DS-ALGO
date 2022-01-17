0b639d7d62aac00cec9f98624158b823f96c8861
gcajay95 <gcajay95@gmail.com>
2022-01-17 14:02:20 -0800

    largest width in a tree

---
+++ Algorithms/Tree/largest_width_of_the_tree.py
@@ -0,0 +1,88 @@
+
+
+from collections import deque
+from difflib import restore
+class Node:
+    def __init__(self, val ) -> None:
+        self.val = val
+        self.right = None
+        self.left = None
+
+    def insert(self, item):
+        new_node = Node(item)
+        if self.val is None:
+            self.val = new_node
+            return
+
+        if item < self.val:
+            if self.left:
+                self.left.insert(item)
+            else:
+                self.left = new_node
+
+        else:
+            if self.right:
+                self.right.insert(item)
+            else:
+                self.right = new_node
+
+    def in_order(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order()
+        print(self.val)
+        elements.append(self.val)
+        if self.right:
+            elements += self.right.in_order()
+
+        return(elements)
+
+node1 = Node(1)
+node1.left = Node(3)
+node1.right = Node(2)
+
+node1.left.left = Node(5)
+node1.right.right = Node(9)
+
+node1.left.left.left = Node(6)
+node1.right.right.right = Node(7)
+
+
+def helper(root):
+    if root is None:
+        return []
+
+    result = []
+    q = deque()
+    q.append((root, 1))
+
+
+    while q:
+
+        len_q = len(q)
+
+        temp = []
+        first = None
+        last = None
+        for _ in range(len_q):
+            (node,id) = q.popleft()
+
+            if node.left:
+                q.append((node.left,2*id))
+            if node.right:
+                q.append((node.right, 2*id+1))
+            
+            last = id
+
+            if first is None:
+                first = id
+
+        result.append(last-first+1)
+
+    return max(result)       
+
+
+
+store = helper(node1)
+print(store)
