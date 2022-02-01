eabffa8e99954f6dc8f1aea98d4c3ed3afa1c58b
gcajay95 <gcajay95@gmail.com>
2022-01-31 17:40:06 -0800

    DP

--- Algorithms/Dynamic Programming/interleavign_string_DP5.py
+++ Algorithms/Dynamic Programming/interleavign_string_DP5.py
@@ -6,7 +6,17 @@ s1 = "aabaac"
 s2 = "aadaaeaaf"
 s3 = "aadaaeaabaafaac"
 
-dptable = [[False]*(len(s2)+1) for _ in range(len(s1)+1)]
+len1, len2 , len3 = len(s1), len(s2), len(s3)
+
+# if len1 + len2 != len3:
+#     return False
+
+# if len1 == 0:
+#     return s2 == s3
+# if len2 == 0:
+#     return s1 == s3
+
+dptable = [[False]*(len(s1)+1) for _ in range(len(s2)+1)]
 
 dptable[0][0] = True
 
@@ -18,13 +28,32 @@ for row in range(1, len(dptable)):
 
 for row in range(1,len(dptable)):
     for col in range(1, len(dptable[0])):
-        dptable[row][col] = (dptable[row-1][col] and (s2[row-1] == s3[row+col-1])) or (dptable[row][col-1] and (s1[col-1] == s3[row+col-1]))
+
+        dptable[row][col] = ( dptable[row-1][col] and ( s2[row-1] == s3[row+col-1] )) or ( dptable[row][col-1]  and ( s1[col-1] == s3[row+col-1] )) 
+
+for item in dptable:
+    print(item)
+print(dptable[-1][-1])
+
+
+# return dptable[-1][-1]
+
+# dptable = [[False]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+# dptable[0][0] = True
+
+# for col in range(1, len(dptable[0])):
+#     dptable[0][col] = dptable[0][col-1] and (s1[col-1] == s3[col-1])
+
+# for row in range(1, len(dptable)):
+#     dptable[row][0] = dptable[row-1][0] and ( dptable[row-1][0] and s2[row-1] == s3[row-1])
+
+# for row in range(1,len(dptable)):
+#     for col in range(1, len(dptable[0])):
+#         dptable[row][col] = (dptable[row-1][col] and (s2[row-1] == s3[row+col-1])) or (dptable[row][col-1] and (s1[col-1] == s3[row+col-1]))
 
 
         # dptable[row][col] = ( dptable[row-1][col] and ( s1[row-1] == s3[row+col-1] )) or ( dptable[row][col-1] and ( s2[col-1] == s3[row+col-1] )) 
         # or (dptable[row-1[col]] and s1[col-1] == s3[col+row-1])
         # (dptable[row-1][col] and (s1[col-1] == s3[row+col-1])) or (dptable[row][col-1] and (s2[row-1] == s3[row+col-1]))
 
-for item in dptable:
-    print(item)
-print(dptable[-1][-1])
