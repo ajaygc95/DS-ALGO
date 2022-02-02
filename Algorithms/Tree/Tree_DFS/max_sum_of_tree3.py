8576b4848744c4a4188aebef76522b82e215664f
gcajay95 <gcajay95@gmail.com>
2022-02-02 00:01:21 -0800

    max sum of path

---
+++ Algorithms/Tree/Tree_DFS/max_sum_of_tree3.py
@@ -0,0 +1,29 @@
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        
+        if not root: return 
+        
+        max_path = [root.val]
+        
+        def dfs(node):
+            
+            my_max = node.val
+            mytotal = node.val
+            
+            if node.left:
+                left = dfs(node.left)
+                my_max = max(my_max, left + node.val)
+                mytotal = my_max
+                
+            if node.right:
+                right = dfs(node.right)
+                my_max = max(my_max, right + node.val)
+                
+                mytotal = max(mytotal, node.val + right, mytotal + right)
+        
+            max_path[0] = max(mytotal, max_path[0])
+            
+            return my_max
+        
+        dfs(root)
+        return max_path[0]
