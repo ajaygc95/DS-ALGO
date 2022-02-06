fbe4adaf508222812436441ff6b07b817634aadf
gcajay95 <gcajay95@gmail.com>
2022-02-05 17:50:27 -0800

    comniation sum 4

--- Algorithms/Dynamic Programming/combination_sum_4_DP7.py
+++ Algorithms/Dynamic Programming/combination_sum_4_DP7.py
@@ -1,10 +1,16 @@
 
 
 nums = [1,2,3]
-target = 7
+target = 4
 
-dptable = [0]*(len(nums)+1)
+dptable = [0]*(target+1)
 dptable[0] = 1
 
+for i in range(1, target+1):
 
-print(dptable)
+    for num in nums:
+        if i - num >= 0:
+            dptable[i] += dptable[i-num]
+
+
+print(dptable[-1])
