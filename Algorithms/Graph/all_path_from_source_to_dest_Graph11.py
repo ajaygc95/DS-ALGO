0780da3d7b50c02277f431ff426194f3b820a67e
gcajay95 <gcajay95@gmail.com>
2022-02-17 23:05:34 -0800

    Graph 11 all path from source to dest Graph

---
+++ Algorithms/Graph/all_path_from_source_to_dest_Graph11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        n = len(graph)
+        def helper(node, slate):
+            #Base Case/ Leaf Node
+            if node == n-1:
+                slate.append(node)
+                result.append(slate[:])
+                slate.pop()
+            #Recursive Case
+            slate.append(node) 
+            for nbr in graph[node]:
+                helper(nbr, slate)
+            
+            slate.pop()
+            
+        result = []
+        helper(0, [])
+        return result
+    
+        # T(N) --> O (2^(1/3)n) is greater than 1 
+        #      --> Exponentianl 
+        # S(N) --> Same as above
