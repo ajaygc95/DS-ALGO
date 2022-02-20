598da1f4727b765d1993fb003df2f613e0d2408f
gcajay95 <gcajay95@gmail.com>
2022-02-19 17:38:36 -0800

    Graph Saturday :

--- Algorithms/Graph/all_path_from_source_to_dest_Graph11.py
+++ Algorithms/Graph/all_path_from_source_to_dest_Graph11.py
@@ -1,24 +1,39 @@
-class Solution:
-    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+# class Solution:
+#     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
 
-        n = len(graph)
-        def helper(node, slate):
-            #Base Case/ Leaf Node
-            if node == n-1:
-                slate.append(node)
-                result.append(slate[:])
-                slate.pop()
-            #Recursive Case
-            slate.append(node) 
-            for nbr in graph[node]:
-                helper(nbr, slate)
+#         n = len(graph)
+#         def helper(node, slate):
+#             #Base Case/ Leaf Node
+#             if node == n-1:
+#                 slate.append(node)
+#                 result.append(slate[:])
+#                 slate.pop()
+#             #Recursive Case
+#             slate.append(node) 
+#             for nbr in graph[node]:
+#                 helper(nbr, slate)
             
-            slate.pop()
+#             slate.pop()
             
-        # result = []
-        # helper(0, [])
-        # return result
+#         # result = []
+#         # helper(0, [])
+#         # return result
     
-        # # T(N) --> O (2^(1/3)n) is greater than 1 
-        # #      --> Exponentianl 
-        # # S(N) --> Same as above
+#         # # T(N) --> O (2^(1/3)n) is greater than 1 
+#         # #      --> Exponentianl 
+#         # # S(N) --> Same as above
+
+final = 5
+
+
+print(final)
+
+def helper(somehting):
+
+
+    final += 2
+
+    print(final)
+
+helper(2)
+print(final)
---
+++ Algorithms/Graph/paralle_courses_graph11.py
@@ -0,0 +1,54 @@
+from os import times
+
+
+n = 3
+relations = [[1,3],[2,3]]
+
+def outer(n, relations):
+
+    adjlist = [[] for _ in range(n+1)]
+
+    for src, dst in relations:
+        adjlist[src].append(dst)
+
+    visited = [-1]*len(adjlist)
+    departure = [-1]*len(adjlist)
+    timestamp = [0]
+    topsort = []
+
+
+    def helper(node):
+        visited[node] = 1
+
+        for nbr in adjlist[node]:
+            if visited[nbr] == -1:
+                if helper(nbr):
+                    return True
+            elif departure[nbr] == -1:
+                return True
+        
+        topsort.append(node)
+        departure[node] = timestamp[0]
+        timestamp[0] += 1
+
+        return False
+
+
+
+    for v in range(1, len(adjlist)):
+        if visited[v] == -1:
+            if helper(v):
+                return -1
+    topsort.reverse()
+
+    dptable = [1]*(n+1)
+
+    for curr in topsort:
+        for nbr in adjlist[curr]:
+            dptable[nbr] = max(dptable[nbr], 1 + dptable[curr])
+    
+    return dptable
+
+
+store = outer ( n, relations)
+print(store)
