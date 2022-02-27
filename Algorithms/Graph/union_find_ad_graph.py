b5864f0f29296832999aabfc53106e64485028d7
gcajay95 <gcajay95@gmail.com>
2022-02-26 20:09:48 -0800

    graph connected

---
+++ Algorithms/Graph/union_find_ad_graph.py
@@ -0,0 +1,24 @@
+edges = None
+
+component = None#something
+
+
+def find(x):
+
+    curr = x 
+
+    while component[curr] != curr:
+        curr = component[curr]
+    return curr
+
+
+for u, v in edges:
+
+    lu = find(u)
+    lv = find(v)
+
+    if lu != lv:
+        component[v] = component[u]
+    
+
+
