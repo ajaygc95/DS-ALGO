58e840d063a23524a7c4dc1dc223af231195e644
gcajay95 <gcajay95@gmail.com>
2022-03-24 22:54:34 -0700

    coin change revision

--- Algorithms/75 questions/1_two_sum.py
+++ Algorithms/75 questions/1_two_sum.py
@@ -1,25 +1,34 @@
-coins = [1,2,5,7]
+import tarfile
+
+
+coins = [9,1,6]
 amount = 11
+
 result = []
 
 def helper(pos, slate, target):
 
     if target < 0:
-        return
+        return 
 
     if target == 0:
-        print("yes")
         result.append(slate[:])
+
     
     for coin in coins:
-        if target-coin>0:
+        if target-coin >= 0:
+
             slate.append(coin)
+
             helper(pos, slate, target-coin)
+            
             slate.pop()
-        
-helper(0, [], 11)
-print(result)
-    
+
+
+helper(0, [], amount)
+
+print( min(result, key=len))
+
 
 
             
