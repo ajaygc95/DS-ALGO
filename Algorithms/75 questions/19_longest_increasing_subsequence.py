eeab2ad4bce769a1d51256f24b840364eb1a2131
gcajay95 <gcajay95@gmail.com>
2022-05-05 00:43:25 -0700

    75 more

--- Algorithms/75 questions/19_longest_increasing_subsequence.py
+++ Algorithms/75 questions/19_longest_increasing_subsequence.py
@@ -3,22 +3,36 @@ from cmath import inf
 
 
 nums = [0,1,0,3,2,3]
-res = []
 
 
-def helper(arr, pos, prev):
-    
-    if pos == len(arr):
-        return 0
-    
-    taken = -inf
-    
-    nottaken = helper(arr, pos+1, prev)
-    
-    if arr[pos] > prev:
-        taken = 1 + helper(arr, pos+1, arr[pos])
 
-    return max(taken, nottaken)
+dptable = [-inf]*(len(nums))
+dptable[0] = 0
 
-print(helper(nums,0,-inf))
+for i in range(1,len(dptable)):
+    res = dptable[i]
+    for j in range(i):
+
+        if nums[i] > nums[j]:
+            res = 1 + max(res, dptable[j])
+    
+    dptable[i] = res
+
+
+
+# def helper(arr, pos, prev):
+    
+#     if pos == len(arr):
+#         return 0
+    
+#     taken = -inf
+    
+#     nottaken = helper(arr, pos+1, prev)
+    
+#     if arr[pos] > prev:
+#         taken = 1 + helper(arr, pos+1, arr[pos])
+
+#     return max(taken, nottaken)
+
+# print(helper(nums, 0, -inf))
 
