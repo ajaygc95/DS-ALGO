c6bbf1459d71001a279d115d481735cea448c7d2
gcajay95 <gcajay95@gmail.com>
2022-05-21 23:21:00 -0700

    something

--- Algorithms/Graph/Graph_Revision/critical_connections.py
+++ Algorithms/Graph/Graph_Revision/critical_connections.py
@@ -6,29 +6,24 @@ import collections
 edges = [[0,1],[1,2],[2,0],[1,3]]
 n = 4
 
-adjlist = [[]for _ in range(n)]
+adjlist = [[] for _ in range(n)]
 
 for src, dst in edges:
     adjlist[src].append(dst)
     adjlist[dst].append(src)
 
-
-visited = [-1] * n
-timestamp = [0]
-lowestarr = [-1]*n
+visited = [-1]*n
 arrival = [-1]*n
+
+lowestarr = [-1]*n
+timestamp = [0]
 parent = {}
-
 res = []
+parent[0] = None
 def dfs(node):
-    print(arrival, lowestarr)
-
-
-    timestamp[0] += 1
-
-    arrival[node] = timestamp[0]
-    lowestarr[node] = arrival
     visited[node] = 1
+    arrival[node] = timestamp[0]
+    lowestarr[node] = arrival[node]
 
     for nbr in adjlist[node]:
         if visited[nbr] == -1:
@@ -37,14 +32,17 @@ def dfs(node):
         else:
             if nbr != parent[node]:
                 lowestarr[node] = min(arrival[nbr], lowestarr[node])
+
     if lowestarr[node] == arrival[node] and node != 0:
         res.append((node, parent[node]))
-
+    
     return lowestarr[node]
-
 
 dfs(0)
 print(res)
+        
+
+
 
 
 
