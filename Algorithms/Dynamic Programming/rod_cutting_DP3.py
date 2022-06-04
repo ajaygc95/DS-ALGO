5f684f5d4de06ef8051ddd5ab6721758318016d3
gcajay95 <gcajay95@gmail.com>
2022-06-03 23:31:46 -0700

    someting

--- Algorithms/Dynamic Programming/rod_cutting_DP3.py
+++ Algorithms/Dynamic Programming/rod_cutting_DP3.py
@@ -1,19 +1,90 @@
-from tkinter import N
+input = "26"
+def helper(n):
+    if n == 0:
+        return 0
+    
+    helper(n-1)
+
+helper(len(input))
+
+# nums = [1, 5, 8, 9]
+
+# dptable = [0]*(len(nums)+1)
+# dptable[0] = 0
+# dptable[1] = nums[0]
+
+# for i in range(2, len(dptable)):
+#     for j in range(1, i):
+#         dptable[i] = max(nums[j]+dptable[i-j-1], dptable[i])
+
+# print(dptable)
 
 
-price = [1,3,5,6,9]
-n = 4
+
+    
+# res = [0]
+
+# def helper(arr, pos, sum):
+    
+#     if pos > len(arr):
+#         return 
+    
+#     if len(arr) == pos:
+#         res[0] = max(res[0], sum)
+#         return 
+    
+#     for i in range(1,len(arr)):
+        
+#         helper(arr, pos+i, sum+arr[i-1])
+    
+# helper(price, 0, 0)
+# print(res)
 
 
-dptable = [0]*n 
 
-dptable[0] = 0
-dptable[1] = price[0]
 
-for i in range(2, n):
 
-    for j in range(1, i):
 
-        dptable[i]  = max(price[j] + dptable[i-j],dptable[i])
 
-print(dptable[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dptable = [0]*n 
+
+# dptable[0] = 0
+# dptable[1] = price[0]
+
+# for i in range(2, n):
+
+#     for j in range(1, i):
+
+#         dptable[i]  = max(price[j] + dptable[i-j],dptable[i])
+
+# print(dptable[i])
